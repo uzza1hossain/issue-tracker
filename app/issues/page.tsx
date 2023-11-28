ade85c1386372c846c521511da29255081a044e2
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-28 11:53:35 +0600

    ✨ (issues): Add IssueDetialPage

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -8,10 +8,10 @@ import IssueActions from './IssueActions';
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(5000);
+  await delay(1000);
   return (
     <div>
-      <IssueActions/>
+      <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -28,7 +28,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
