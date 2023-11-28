aa4a2d87cad95a86a6fb25b714fefbea58801c44
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-28 20:33:29 +0600

    💄 (issues): Add sytle on the issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import React from 'react';
 
@@ -14,10 +16,15 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   return (
     <div>
-      <p>{issue.title}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex gap={'3'} my={'2'}>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+
       <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createAt.toDateString()}</p>
+      </Card>
     </div>
   );
 };
