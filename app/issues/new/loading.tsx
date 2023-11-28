a4a8f9e28150518acdf5fb53a2dc2c7ff7034e3b
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-28 23:21:47 +0600

    🐛 (issues): Disabled ssr on new issue form

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,9 +1,7 @@
 import { Box } from '@radix-ui/themes';
-import delay from 'delay';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 const LoadingNewIssuePage = async () => {
-  await delay(3000);
   return (
     <Box className="max-w-xl">
       <Skeleton />
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,20 @@
 'use client';
-import axios from 'axios';
-import SimpleMDE from 'react-simplemde-editor';
-import 'easymde/dist/easymde.min.css';
-import { Button, Callout, TextField, Text } from '@radix-ui/themes';
-import React, { useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import { useRouter } from 'next/navigation';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
-import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import axios from 'axios';
+import 'easymde/dist/easymde.min.css';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
