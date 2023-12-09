a49f5978917c1cd35b220e789838f9241cb5ff13
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-12-09 23:19:22 +0600

    ✨ Filter issues

--- app/issues/list/IssuesStatusFilter.tsx
+++ app/issues/list/IssuesStatusFilter.tsx
@@ -2,6 +2,7 @@
 import { Status } from '@prisma/client';
 
 import { Select } from '@radix-ui/themes';
+import { useRouter } from 'next/navigation';
 
 const statuses: { label: string; value?: Status }[] = [
   { label: 'All' },
@@ -11,8 +12,13 @@ const statuses: { label: string; value?: Status }[] = [
 ];
 
 const IssuesStatusFilter = () => {
+  const router = useRouter();
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        const query = status ? '?status=' + status : '';
+        router.push('/issues/list' + query);
+      }}>
       <Select.Trigger placeholder="filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
