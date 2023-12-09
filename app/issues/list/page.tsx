659326622e898478ce6d083dd4dd1f6903a4a2b5
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-12-09 23:41:02 +0600

    ✨ Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -27,8 +27,13 @@ const IssuesPage = async ({
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns.map(column => column.value).includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy,
   });
   return (
     <div>
