8157f587e39f3c25ee44db35820dca348dd5b796
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-12-13 20:09:04 +0600

    ✨ Add metadata

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -5,6 +5,7 @@ import { Status } from '@prisma/client';
 import { Flex } from '@radix-ui/themes';
 import IssueActions from './IssueActions';
 import IssueTable, { IssueQuery, columnNames } from './IssueTable';
+import { Metadata } from 'next';
 
 interface Props {
   searchParams: IssueQuery;
@@ -48,5 +49,10 @@ const IssuesPage = async ({ searchParams }: Props) => {
 };
 export const dynamic = 'force-dynamic';
 // export const revalidate = 60;
+export const metadata: Metadata = {
+  title: 'Issue Tracker - Issue List',
+  description: 'View all project issues',
+};
 
 export default IssuesPage;
+
